6ed972272a6865959e593835afe6c73de93c9da1
patorma <pocontrera@ing.ucsc.cl>
2020-04-21 01:50:03 -0400

    una forma de ver los hooks con useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [stateCar, setStateCar] = useState(false);
+  const encenderApagar = () => {
+    // actualizar el estado con setStateCar
+    // con ! le digo que mando lo contrario del estado
+    setStateCar(!stateCar);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>El cooche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <button onClick={encenderApagar}>Encender / Apagar</button>
       </header>
     </div>
   );
